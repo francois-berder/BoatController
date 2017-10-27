0cbf974828aac25041aa57b180c233071adbd587
Francois Berder <fberder@outlook.fr>
2017-10-27 09:46:35 +0100

    mcu: p24fj128gb202: Implement timer interrupt handling
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- mcu/p24fj128gb202/periph/timer.c
+++ mcu/p24fj128gb202/periph/timer.c
@@ -21,29 +21,111 @@
 #include "periph/timer.h"
 #include "periph_conf.h"
 
-void timer_configure(unsigned int timer_num, uint8_t prescaler, uint16_t period)
+void __attribute__ ((weak)) timer2_callback(void)
+{
+
+}
+
+void __attribute__ ((weak)) timer3_callback(void)
+{
+
+}
+
+void __attribute__ ((weak)) timer4_callback(void)
+{
+
+}
+
+void __attribute__ ((weak)) timer5_callback(void)
+{
+
+}
+
+void __attribute__ ((interrupt, no_auto_psv)) _T2Interrupt(void)
+{
+    timer2_callback();
+    IFS0 &= ~ _IFS0_T2IF_MASK;
+}
+
+void __attribute__ ((interrupt, no_auto_psv)) _T3Interrupt(void)
+{
+    timer3_callback();
+    IFS0 &= ~ _IFS0_T3IF_MASK;
+}
+
+void __attribute__ ((interrupt, no_auto_psv)) _T4Interrupt(void)
+{
+    timer4_callback();
+    IFS1 &= ~ _IFS1_T4IF_MASK;
+}
+
+void __attribute__ ((interrupt, no_auto_psv)) _T5Interrupt(void)
+{
+    timer5_callback();
+    IFS1 &= ~ _IFS1_T5IF_MASK;
+}
+
+void timer_configure(unsigned int timer_num,
+                     uint8_t prescaler, uint16_t period,
+                     uint8_t enable_interrupt)
 {
     switch (timer_num) {
     case TIMER_2:
         T2CON = (prescaler << _T2CON_TCKPS_POSITION) & _T2CON_TCKPS_MASK;
         PR2 = period;
         TMR2 = 0;
+        IFS0 &= ~ _IFS0_T2IF_MASK;
         break;
     case TIMER_3:
         T3CON = (prescaler << _T3CON_TCKPS_POSITION) & _T3CON_TCKPS_MASK;
         PR3 = period;
         TMR3 = 0;
+        IFS0 &= ~ _IFS0_T3IF_MASK;
         break;
     case TIMER_4:
         T4CON = (prescaler << _T4CON_TCKPS_POSITION) & _T4CON_TCKPS_MASK;
         PR4 = period;
         TMR4 = 0;
+        IFS1 &= ~ _IFS1_T4IF_MASK;
         break;
     case TIMER_5:
         T5CON = (prescaler << _T5CON_TCKPS_POSITION) & _T5CON_TCKPS_MASK;
         PR5 = period;
         TMR5 = 0;
+        IFS1 &= ~ _IFS1_T5IF_MASK;
         break;
+    }
+
+    if (enable_interrupt) {
+        switch (timer_num) {
+        case TIMER_2:
+            IEC0 |= _IEC0_T2IE_MASK;
+            break;
+        case TIMER_3:
+            IEC0 |= _IEC0_T3IE_MASK;
+            break;
+        case TIMER_4:
+            IEC1 |= _IEC1_T4IE_MASK;
+            break;
+        case TIMER_5:
+            IEC1 |= _IEC1_T5IE_MASK;
+            break;
+        }
+    } else {
+        switch (timer_num) {
+        case TIMER_2:
+            IEC0 &= ~_IEC0_T2IE_MASK;
+            break;
+        case TIMER_3:
+            IEC0 &= ~_IEC0_T3IE_MASK;
+            break;
+        case TIMER_4:
+            IEC1 &= ~_IEC1_T4IE_MASK;
+            break;
+        case TIMER_5:
+            IEC1 &= ~_IEC1_T5IE_MASK;
+            break;
+        }
     }
 }
 
