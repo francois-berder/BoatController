595308096de22bc36704c08c8776d3dce9712e20
Francois Berder <fberder@outlook.fr>
2017-10-27 23:59:27 +0100

    mcu: p24fj128gb202: Implement watchdog interface
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ mcu/p24fj128gb202/periph/watchdog.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017  Francois Berder <fberder@outlook.fr>
+ *
+ * This file is part of pic24-framework.
+ *
+ * pic24-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pic24-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pic24-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <xc.h>
+#include "periph/watchdog.h"
+
+void watchdog_enable(void)
+{
+    RCON |= _RCON_SWDTEN_MASK;
+}
+
+void watchdog_disable(void)
+{
+    RCON &= ~_RCON_SWDTEN_MASK;
+}
+
+void watchdog_kick(void)
+{
+    __asm__ __volatile__ ("clrwdt");
+}
