0639c8f29474d87e1a1f0b0361f3033654de6fed
Francois Berder <fberder@outlook.fr>
2017-10-31 09:39:30 +0000

    firmware: Add Makefile, main and configuration bits
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ apps/firmware/src/main.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2017  Francois Berder <fberder@outlook.fr>
+ *
+ * This file is part of pic24-framework.
+ *
+ * pic24-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pic24-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pic24-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* CONFIG1 */
+#pragma config WDTPS = PS1024
+#pragma config FWPSA = PR32
+#pragma config WINDIS = OFF
+#pragma config FWDTEN = SWON
+#pragma config ICS = PGx2
+#pragma config LPCFG = OFF
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = OFF
+
+/* CONFIG2 */
+#pragma config POSCMD = NONE
+#pragma config WDTCLK = LPRC
+#pragma config OSCIOFCN = ON
+#pragma config FCKSM = CSDCMD
+#pragma config FNOSC = FRC
+#pragma config ALTRB6 = RETAIN
+#pragma config ALTCMPI = CxINC_RB
+#pragma config WDTCMX = WDTCLK
+#pragma config IESO = OFF
+
+/* CONFIG3 */
+#pragma config WPFP = WPFP127
+#pragma config SOSCSEL = OFF
+#pragma config WDTWIN = PS25_0
+#pragma config PLLSS = PLL_FRC
+#pragma config BOREN = ON
+#pragma config WPDIS = WPDIS
+#pragma config WPCFG = WPCFGDIS
+#pragma config WPEND = WPENDMEM
+
+/* CONFIG4 */
+#pragma config DSWDTPS = DSWDTPS1F
+#pragma config DSWDTOSC = LPRC
+#pragma config DSBOREN = ON
+#pragma config DSWDTEN = ON
+#pragma config DSSWEN = ON
+#pragma config PLLDIV = PLL4X
+#pragma config I2C1SEL = DISABLE
+#pragma config IOL1WAY = OFF
+
+int main(void)
+{
+    while (1) {
+
+    }
+
+    return 0;
+}
