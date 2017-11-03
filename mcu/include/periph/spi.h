970dd9e3436c0a441639f7e99f13f56593f5f3f8
Francois Berder <fberder@outlook.fr>
2017-11-03 23:58:39 +0000

    mcu: Define spi_fast_read
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- mcu/include/periph/spi.h
+++ mcu/include/periph/spi.h
@@ -76,6 +76,15 @@ void spi_disable(unsigned int spi_num);
 void spi_transfer(unsigned int spi_num, const void *tx_buffer, void *rx_buffer, uint32_t length);
 
 /**
+ * @brief Make a fast read from SPI
+ *
+ * @param[in] spi_num Index of the SPI module
+ * @param[in] rx_buffer Array of bytes to receive
+ * @param[in] length Number of bytes to receive/send, must be a multiple of 32
+ */
+void spi_fast_read(unsigned int spi_num, void *rx_buffer, uint32_t length);
+
+/**
  * @brief Power-up a SPI module
  *
  * @param[in] spi_num Index of the SPI module
