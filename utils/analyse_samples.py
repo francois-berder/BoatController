a7de7946331ad715ff7c34529c798027eeafb75c
Francois Berder <fberder@outlook.fr>
2017-12-10 12:00:56 +0000

    utils: Add low pass filter to see effects on stdev
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- utils/analyse_samples.py
+++ utils/analyse_samples.py
@@ -6,7 +6,7 @@ import statistics
 import sys
 
 HEADERS = ['accel x', 'accel y', 'accel z', 'gyro x', 'gyro y', 'gyro z']
-FORMAT_STR = '{0:10} | {1:20} | {2:20}'
+FORMAT_STR = '{0:15} | {1:20} | {2:20}'
 
 if len(sys.argv) < 2:
     print('{} <path-to-sample-file>'.format(sys.argv[0]))
@@ -22,6 +22,19 @@ results = [()] * 6
 for i, c in enumerate(columns):
     results[i] = (HEADERS[i], statistics.mean(c), statistics.stdev(c))
 
-print(FORMAT_STR.format('name', 'average', 'standard deviation'))
+h = FORMAT_STR.format('name', 'average', 'standard deviation')
+print(h)
+print('-' * len(h))
 for r in results:
     print(FORMAT_STR.format(r[0], r[1], r[2]))
+
+# Take average of 4 samples, compute mean (should not change and) standard deviation
+results = [()] * 6
+ax = columns[0]
+l = int(len(ax) / 4)
+for i, c in enumerate(columns):
+    s = [0] * l
+    for j in range(l):
+        s[j] = c[4 * j] + c[4 * j + 1] + c[4 * j + 2] + c[4 * j + 3]
+        s[j] /= 4
+    print(FORMAT_STR.format('smooth ' + HEADERS[i], statistics.mean(s), statistics.stdev(s)))
