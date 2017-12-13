36db1eb1ecfd6fc1e90532bf5bb925c6112aca1e
Francois Berder <fberder@outlook.fr>
2017-12-13 10:39:03 +0000

    firmware: Log radio & output frames to SD card
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- apps/firmware/src/controller.c
+++ apps/firmware/src/controller.c
@@ -17,14 +17,68 @@
  * along with pic24-framework.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
+#include "block_storage.h"
 #include "controller.h"
+#include "fat16/fat16.h"
 #include "mcu.h"
 #include "output.h"
+#include "periph/crypto.h"
 #include "radio.h"
+
+static int fd = -1;
+
+static void open_log_file(void)
+{
+    char filename[13];
+    unsigned int i;
+
+    crypto_power_up();
+    crypto_enable();
+    crypto_get_random(filename, 8);
+    crypto_disable();
+    crypto_power_down();
+
+    for (i = 0; i < 8; ++i)
+        filename[i] = 'A' + (filename[i] & 0xF);
+
+    filename[8] = '.';
+    filename[9] = 'T';
+    filename[10] = 'X';
+    filename[11] = 'T';
+    filename[12] = '\0';
+
+    fd = fat16_open(filename, 'w');
+    if (fd < 0) {
+        printf("Cannot log to file %s\n", filename);
+    } else {
+        printf("Starting logging to file %s\n", filename);
+    }
+
+    /*
+        * Flush cache now to ensure that file will exist on the SD card
+        * even if no radio frames are found later.
+        */
+    block_storage_flush();
+}
+
+static void log_frame(struct radio_frame_t rf, struct output_frame_t of)
+{
+    char buffer[128];
+    int ret = sprintf(buffer, "%u, %u, %u, %u, %u, %u\n",
+                      rf.dir, rf.speed,
+                      of.left_rudder, of.right_rudder, of.left_motor, of.right_motor);
+    if (ret >= 0)
+        fat16_write(fd, buffer, ret);
+}
 
 void controller_run(struct board_config_t config)
 {
-    (void)config;
+    unsigned int counter = 0;
+
+    if (config.sdcard_enabled) {
+        open_log_file();
+    }
 
     while (1) {
         if (radio_has_frame()) {
@@ -38,6 +92,17 @@ void controller_run(struct board_config_t config)
             output_frame.right_motor = radio_frame.speed;
 
             output_set_frame(output_frame);
+
+            if (fd >= 0) {
+                log_frame(radio_frame, output_frame);
+                ++counter;
+
+                /* Ensure that log file is periodically saved to SD card */
+                if (counter == 100) {
+                    counter = 0;
+                    block_storage_flush();
+                }
+            }
         }
 
         /* Wait 5ms */
