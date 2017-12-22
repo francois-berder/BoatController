71b939455e750aa61662805325cedefb97ecf516
Francois Berder <fberder@outlook.fr>
2017-12-22 21:46:56 +0100

    mpu6050_collect: Blink LED during samples collection
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- apps/mpu6050_collect/README.md
+++ apps/mpu6050_collect/README.md
@@ -1,9 +1,10 @@
 ## BoatController - mpu6050 sample collection
 
-This firmware is used to collect samples from the MPU6050. These samples are written on the sdcard in
-the file ```/MPU6050.TXT```.
+This firmware is used to collect samples from the MPU6050. These samples are written on the sdcard in the file ```/MPU6050.TXT```.
 
 Each line of the file represents a sample in the following format:
 ```
 accel.x, accel.y, accel.z, gyro.x, gyro.y, gyro.z
 ```
+
+During samples collection, the LED must be blinking. If the LED is solid, something went wrong during the initialisation.
--- apps/mpu6050_collect/src/main.c
+++ apps/mpu6050_collect/src/main.c
@@ -89,6 +89,8 @@
 #define SCK_PIN         (GPIO_PIN(PORT_B, 6))
 #define CS_PIN          (GPIO_PIN(PORT_B, 7))
 
+#define LED_PIN         (GPIO_PIN(PORT_B, 5))
+
 #ifndef FIRMWARE_VERSION
 #define FIRMWARE_VERSION "dev"
 #endif
@@ -173,6 +175,9 @@ int main(void)
     timer1_power_up();
     timer1_configure(TIMER1_PRESCALER_1, 4000, 1);
     timer1_start();
+
+    /* Turn on LED pin */
+    gpio_init_out(LED_PIN, 1);
 
     /* Configure uart */
     gpio_init_out(UART_TX_PIN, 1);
@@ -287,6 +292,7 @@ int main(void)
         printf(".");
 
         if (counter > 50) {
+            gpio_toggle(LED_PIN);
             printf("saving to sdcard\n");
             sdcard_cache_flush();
             counter = 0;
