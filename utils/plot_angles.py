55f61665e8a28d32f0430dcadc457d714c13be96
Francois Berder <fberder@outlook.fr>
2017-12-24 15:06:50 +0100

    utils: Adapt script to new layout of data
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- utils/plot_angles.py
+++ utils/plot_angles.py
@@ -14,10 +14,10 @@ GYROSCOPE_SENSITIVITY = 65.5
 alpha = 0.98
 
 if len(sys.argv) < 2:
-    print('{} <path-to-sample-file>'.format(sys.argv[0]))
+    print('{} <path-to-directory>'.format(sys.argv[0]))
     exit(-1)
 
-f = open(sys.argv[1])
+f = open('{}/{}'.format(sys.argv[1], 'MPU6050.TXT')
 reader = csv.reader(f)
 rows = [list(map(float, r)) for r in reader]
 columns = [list(tmp) for tmp in zip(*rows)]
@@ -35,11 +35,10 @@ for i in range(1, steps):
 
     acc_x = math.atan2(columns[0][i], columns[2][i]) * 180.0 / math.pi
     acc_y = math.atan2(columns[1][i], columns[2][i]) * 180.0 / math.pi
-    acc_z = math.atan2(columns[0][i], columns[1][i]) * 180.0 / math.pi
 
     roll[i] = alpha * (roll[i - 1] + gyro_x) - (1.0 - alpha) * acc_x
     pitch[i] = alpha * (pitch[i - 1] + gyro_y) + (1.0 - alpha) * acc_y
-    yaw[i] = alpha * (yaw[i - 1] + gyro_z) #+ (1.0 - alpha) * acc_z
+    yaw[i] = alpha * (yaw[i - 1] + gyro_z)
 
 plt.plot(roll, label='roll')
 plt.plot(pitch, label='pitch')
