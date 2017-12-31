9809ed962c84e3869ceb31e3aa5180af20198d96
Francois Berder <fberder@outlook.fr>
2017-12-31 13:42:04 +0100

    firmware: Refactor output module to reduce interval between commands
    In the previous design, command to left/right servo/motors
    were sent 4ms apart allowing for a simpler design.
    However, that would mean that the motor on the left would apply
    a command before the motor on the right.
    
    This new design partially fixes this issue. Commands to servos
    are sent at the same time, followed 2.5ms later by motor
    commands. It was not possible to fix the issue due to a lack
    of timers (timer 3 & 4 are the only ones available).
    
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- apps/firmware/src/output.c
+++ apps/firmware/src/output.c
@@ -31,11 +31,10 @@
 #define RIGHT_MOTOR_PIN         (GPIO_PIN(PORT_A, 3))
 
 enum OUTPUT_STATE {
-    FETCH_DATA,
-    LEFT_RUDDER,
-    RIGHT_RUDDER,
-    LEFT_MOTOR,
-    RIGHT_MOTOR
+    START_COMMAND_SERVO,
+    END_COMMAND_SERVO,
+    START_COMMAND_MOTOR,
+    END_COMMAND_MOTOR
 };
 
 static volatile enum OUTPUT_STATE state;
@@ -45,58 +44,65 @@ void timer3_callback(void)
 {
     /* Move on to next state */
     ++state;
-    if (state > RIGHT_MOTOR)
-        state = FETCH_DATA;
+    if (state > END_COMMAND_MOTOR)
+        state = START_COMMAND_SERVO;
 
     switch (state) {
-    case FETCH_DATA:
+    case START_COMMAND_SERVO:
         current_frame = next_frame;
-        break;
-    case LEFT_RUDDER:
-        gpio_write(LEFT_RUDDER_PIN, 1);
-        timer_configure(TIMER_4, TIMER4_PRESCALER_1, current_frame.left_rudder, 1);
-        break;
-    case RIGHT_RUDDER:
-        gpio_write(RIGHT_RUDDER_PIN, 1);
-        timer_configure(TIMER_4, TIMER4_PRESCALER_1, current_frame.right_rudder, 1);
-        break;
-    case LEFT_MOTOR:
-        gpio_write(LEFT_MOTOR_PIN, 1);
-        timer_configure(TIMER_4, TIMER4_PRESCALER_1, current_frame.left_motor, 1);
-        break;
-    case RIGHT_MOTOR:
-        gpio_write(RIGHT_MOTOR_PIN, 1);
-        timer_configure(TIMER_4, TIMER4_PRESCALER_1, current_frame.right_motor, 1);
-        break;
-    }
 
-    if (state != FETCH_DATA) {
+        /* Configure timer 3 & 4 */
         timer_power_up(TIMER_4);
+        timer_configure(TIMER_3, TIMER3_PRESCALER_1, current_frame.left_rudder, 1);
+        timer_configure(TIMER_4, TIMER4_PRESCALER_1, current_frame.right_rudder, 1);
+        timer_start(TIMER_3);
         timer_start(TIMER_4);
+
+        gpio_write(LEFT_RUDDER_PIN, 1);
+        gpio_write(RIGHT_RUDDER_PIN, 1);
+        break;
+    case END_COMMAND_SERVO:
+        gpio_write(LEFT_RUDDER_PIN, 0);
+
+        /* Configure timer 3 to trigger in 2.5ms - data0 */
+        timer_configure(TIMER_3, TIMER3_PRESCALER_1, 10000 - current_frame.left_rudder, 1);
+        timer_start(TIMER_3);
+        break;
+    case START_COMMAND_MOTOR:
+        /* Configure timer 3 & 4 */
+        timer_power_up(TIMER_4);
+        timer_configure(TIMER_3, TIMER3_PRESCALER_1, current_frame.left_motor, 1);
+        timer_configure(TIMER_4, TIMER4_PRESCALER_1, current_frame.right_motor, 1);
+        timer_start(TIMER_3);
+        timer_start(TIMER_4);
+
+        gpio_write(LEFT_MOTOR_PIN, 1);
+        gpio_write(RIGHT_MOTOR_PIN, 1);
+        break;
+    case END_COMMAND_MOTOR:
+        gpio_write(LEFT_MOTOR_PIN, 0);
+
+        /* Configure timer 3 to trigger in 17.5ms - data2 */
+        timer_configure(TIMER_3, TIMER3_PRESCALER_8, 8750 - (current_frame.left_motor >> 3), 1);
+        timer_start(TIMER_3);
+        break;
     }
 }
 
 void timer4_callback(void)
 {
     switch (state) {
-    case FETCH_DATA:
-        break;
-    case LEFT_RUDDER:
-        gpio_write(LEFT_RUDDER_PIN, 0);
-        break;
-    case RIGHT_RUDDER:
+    case START_COMMAND_SERVO:
+    case END_COMMAND_SERVO:
         gpio_write(RIGHT_RUDDER_PIN, 0);
         break;
-    case LEFT_MOTOR:
-        gpio_write(LEFT_MOTOR_PIN, 0);
-        break;
-    case RIGHT_MOTOR:
+    case START_COMMAND_MOTOR:
+    case END_COMMAND_MOTOR:
         gpio_write(RIGHT_MOTOR_PIN, 0);
         break;
     }
 
     timer_stop(TIMER_4);
-    timer_power_down(TIMER_4);
 }
 
 void output_configure(void)
@@ -104,31 +110,35 @@ void output_configure(void)
     /* Ensure that no timers are running during configuration */
     timer_stop(TIMER_3);
     timer_stop(TIMER_4);
-
-    /* Configure timer 3 to call callback every 4 ms */
     timer_power_up(TIMER_3);
-    timer_configure(TIMER_3, TIMER3_PRESCALER_8, 2000, 1);
+    timer_power_up(TIMER_4);
 
-    state = RIGHT_MOTOR;
+    gpio_init_out(LEFT_RUDDER_PIN, 0);
+    gpio_init_out(RIGHT_RUDDER_PIN, 0);
+    gpio_init_out(LEFT_MOTOR_PIN, 0);
+    gpio_init_out(RIGHT_MOTOR_PIN, 0);
 
     current_frame.left_rudder = NEUTRAL_POS;
     current_frame.right_rudder = NEUTRAL_POS;
     current_frame.left_motor = NEUTRAL_POS;
     current_frame.right_motor = NEUTRAL_POS;
     next_frame = current_frame;
-
-    gpio_init_out(LEFT_RUDDER_PIN, 0);
-    gpio_init_out(RIGHT_RUDDER_PIN, 0);
-    gpio_init_out(LEFT_MOTOR_PIN, 0);
-    gpio_init_out(RIGHT_MOTOR_PIN, 0);
 }
 
 void output_enable(void)
 {
+    timer_stop(TIMER_3);
+    timer_stop(TIMER_4);
+
+    state = END_COMMAND_MOTOR;
+
     gpio_write(LEFT_RUDDER_PIN, 0);
     gpio_write(RIGHT_RUDDER_PIN, 0);
     gpio_write(LEFT_MOTOR_PIN, 0);
     gpio_write(RIGHT_MOTOR_PIN, 0);
+
+    /* Configure timer 3 to trigger as soon as it starts */
+    timer_configure(TIMER_3, TIMER3_PRESCALER_1, 1, 1);
     timer_start(TIMER_3);
 }
 
