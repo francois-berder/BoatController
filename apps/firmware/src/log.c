bce056bc21a16edc978a4f1190e69b46d51a430f
Francois Berder <fberder@outlook.fr>
2018-05-24 11:03:44 +0100

    firmware: Add missing license in log source file
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- apps/firmware/src/log.c
+++ apps/firmware/src/log.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018  Francois Berder <fberder@outlook.fr>
+ *
+ * This file is part of pic24-framework.
+ *
+ * pic24-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pic24-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pic24-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include "fat16/fat16.h"
 #include "log.h"
